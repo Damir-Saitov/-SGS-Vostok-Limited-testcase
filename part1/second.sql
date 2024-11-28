e42df93157bc497f53118035a980ff598becbc32
Damir Saitov <damir_saitov1985@mail.ru>
2024-11-28 06:27:13 +0400

    Добавлены альтернативные решения запросов

--- part1/second.sql
+++ part1/second.sql
@@ -1,1 +1,27 @@
-SELECT * FROM Operations FOR JSON PATH;
+SELECT * FROM Containers FOR JSON PATH;
+
+
+-- Альтернативный вариант
+DECLARE @result varchar(MAX);
+SELECT @result = ISNULL(@result, '') + '{' + (
+    '"id":' + CONVERT(VARCHAR(20), id)
+    + ',' +
+    '"number":' + CONVERT(VARCHAR(20), number)
+    + ',' +
+    '"type":' + '"' + type + '"'
+    + ',' +
+    '"length":' + CONVERT(VARCHAR(20), length)
+    + ',' +
+    '"width":' + CONVERT(VARCHAR(20), width)
+    + ',' +
+    '"height":' + CONVERT(VARCHAR(20), height)
+    + ',' +
+    '"weight":' + CONVERT(VARCHAR(20), weight)
+    + ',' +
+    '"empty":' + CONVERT(VARCHAR(20), empty)
+    + ',' +
+    '"receipt_date":' + '"' + CONVERT(VARCHAR(20), receipt_date, 126) + '"'
+) + '},'
+FROM Containers;
+
+SELECT result = '[' + substring(@result, 0, len(@result)) + ']';
--- part1/third.sql
+++ part1/third.sql
@@ -6,3 +6,26 @@ SET @container_id = 1;
 SELECT * FROM Operations
 WHERE (container_id = @container_id)
 FOR JSON PATH;
+
+
+-- Альтернативный вариант
+DECLARE @result varchar(MAX);
+SELECT @result = ISNULL(@result, '') + '{' + (
+    '"id":' + CONVERT(VARCHAR(20), id)
+    + ',' +
+    '"container_id":' + CONVERT(VARCHAR(20), container_id)
+    + ',' +
+    '"start_date":' + '"' + CONVERT(VARCHAR(20), start_date, 126) + '"'
+    + ',' +
+    '"end_date":' + '"' + CONVERT(VARCHAR(20), end_date, 126) + '"'
+    + ',' +
+    '"type":' + '"' + type + '"'
+    + ',' +
+    '"operators_fullname":' + '"' + operators_fullname + '"'
+    + ',' +
+    '"inspection_place":' + '"' + inspection_place + '"'
+) + '},'
+FROM Operations
+WHERE (container_id = @container_id);
+
+SELECT result = '[' + substring(@result, 0, len(@result)) + ']';
